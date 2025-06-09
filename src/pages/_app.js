e19e2756d70637c3aa5f96da0a78ee1049f1a00b
Sparkie <78498155+Serenawaifu@users.noreply.github.com>
2025-06-10 03:40:46 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,9 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
 
+/**
+ * Wraps all pages with global layout for consistent header, footer, and theming.
+ */
 export default function App({ element }) {
   return <Layout>{element}</Layout>;
 }
