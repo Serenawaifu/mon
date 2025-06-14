c423406a138b7e442209d9639c6ed529629f191d
Sparkie <78498155+Serenawaifu@users.noreply.github.com>
2025-06-15 03:13:23 +0530

    Update auth.js

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -3,12 +3,17 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
 import AuthForm from "../components/Auth/AuthForm";
+import WalletConnectButton from "../components/Auth/WalletConnectButton";
 
 export default function AuthPage() {
   return (
     <Layout>
       <main className="min-h-screen flex flex-col items-center justify-center bg-white px-6">
-        <AuthForm />
+        <h1 className="text-2xl font-bold mb-6">Authenticate</h1>
+        <WalletConnectButton />
+        <div className="my-6 w-full max-w-md">
+          <AuthForm />
+        </div>
       </main>
     </Layout>
   );
